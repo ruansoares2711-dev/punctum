dd2dd193b9aa5233dae22af53c2f3d834bc9ec2a
Ruan Soares Silva <ruansoares2711@gmail.com>
2026-06-02 21:06:43 -0300

    refactor: restructure API handler and remove deprecated files

--- api/handler.ts
+++
@@ -1,6 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-
-// Import do entry point do servidor gerado pelo TanStack Start
-const serverEntry = await import("../dist/server/index.js");
-
-export default serverEntry.default;
